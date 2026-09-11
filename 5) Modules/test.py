80bb3457308071c02ad7c24bc71c147a670857aa
Ritika Bhati <ritika452044@gmail.com>
2026-09-11 15:22:48 +0530

    updated file

---
+++ 5) Modules/test.py
@@ -0,0 +1,10 @@
+# from package.maths import *
+# print(addition(2,4))
+# print(subtraction(2,1))
+
+
+from package.maths import *
+from package.subpackages.multi import multiplication
+print(addition(2,4))
+print(subtraction(2,1))
+print(multiplication(2,8))
